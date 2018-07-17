8dc7638e67128df964e7f488e565ac3888fa17cf
Ingrid Bau <ingrid.bau@gmail.com>
2018-07-17 10:43:02 -0400

    add files to the repo

---
+++ README.md
@@ -0,0 +1,60 @@
+### Insight Data Engineer Pharmacy Counting Challenge
+
+#### Python standard libraries used - 
+
+- csv:
+  To read and process the input file.
+
+- time:
+  To track time spent in each step.
+
+- collections/defaultdict:
+  Instead of the default 'dict' object, I used 'defaultdict' to store prescriber and drug cost information for each unique drug.
+
+- itertools/chain:
+  To merge 2 defaultdict objects into the final drug table.
+
+
+#### pharmacy_count.py - 
+
+In addition to the first (read and test input file) and the last (write and test output file) parts, this code includes 4 steps:
+
+    1. Data preparation and cleaning to create a sorted [drug_name, prescriber_name, drug_cost] list
+    2. Calculate number of unique prescribers for each drug {dict with drug_name as the key}
+    3. Calculate total cost for each drug {dict with drug_name as the key}
+    4. Combine two dictionaries and sort the final drug table
+
+
+#### Environment - 
+
+Sierra v.10.12.6
+MacBook Air mid 2011 
+Processor 1.7 GHz Intel Core i5
+Memory 4 GB 1333 MHz DDR3
+
+
+#### Performance test - 
+
+I tested the code with input data of 5, 1k, and 100k records.  All three tests finished within a reasonable timeframe.  I also tried the 24.5m records dataset, but it failed to finish in 20 minutes.  From the time log below, we can see that step 2 "prescriber number count" took the longest, followed by step 1 “data preparation”.  To further improve performance when using large datasets, we can find more efficient ways to read in the raw data, and/or to store prescriber information for each drug.  Going forward, we can also try to split large dataset for parallel computing and run the code on environments with better hardware specs.
+
+    - 5 records:
+    > Data preparation took 0.0 seconds to run!
+    > Prescriber number took 0.0002 seconds to run!
+    > Drug cost took 0.0001 seconds to run!
+    > Final sorted table took 0.0001 seconds to run!
+    > In total pharmacy_counting.py took 0.001 seconds to run!
+
+    - 1k records:
+    > Data preparation took 0.0013 seconds to run!
+    > Prescriber number took 0.0008 seconds to run!
+    > Drug cost took 0.0007 seconds to run!
+    > Final sorted table took 0.0005 seconds to run!
+    > In total pharmacy_counting.py took 0.0043 seconds to run!
+
+
+    - 100k records:
+    > Data preparation took 0.4207 seconds to run!
+    > Prescriber number took 0.6829 seconds to run!
+    > Drug cost took 0.1211 seconds to run!
+    > Final sorted table took 0.0047 seconds to run!
+    > In total pharmacy_counting.py took 1.2345 seconds to run!
---
+++ src/pharmacy_counting.py
@@ -0,0 +1,132 @@
+import csv
+import time
+from collections import defaultdict
+from itertools import chain
+
+# import raw data into input_file
+try:
+    with open("./input/itcont.txt") as f:
+        input_file = csv.reader(f)
+        header = next(input_file) # skip header
+        input_file = list(input_file)
+except IOError as e:
+    print('File does not exist or no permissions.')
+    exit()
+
+
+'''
+Data preparing and cleaning:
+
+Create a 2-D list with cleaned and sorted records from the raw txt file.
+Each row includes the drug name, concatenated prescriber name, and drug cost from the raw data.
+
+Return [input_records]: 
+columns: [drug_name, prescriber_name(last+first, upper-cased), drug_cost]
+rows: sorted by drug_name then by prescriber_name
+'''
+start_time = time.time()
+
+input_records=[]
+
+for records in input_file:
+    input_records.append([records[3], (records[1]+' '+records[2]).upper(), records[4]])
+
+input_records.sort(key=lambda x: (x[0], x[1]))
+
+data_time = time.time()
+print ('Data preparation took {0} seconds to run!'.format(round((data_time-start_time),4)))
+
+
+'''
+Calculate number of prescribers (num_prescriber):
+
+Create a dictionary contains the number of unique prescribers for each drug
+
+Return {prescriber_num}: 
+key: 'drug_name'
+value: float(num_prescriber)
+a. first create a list of unique prescriber names for each drug
+b. then get the length of each list from a.
+'''
+from collections import defaultdict
+
+# create a dictionary defaults to list
+prescriber_number = defaultdict(list) 
+
+# step a.
+for k, v1, v2 in input_records:
+    if v1 not in prescriber_number[k]:
+        prescriber_number[k].append(v1)
+# step b.
+for k in prescriber_number.keys():
+    prescriber_number[k] = len(prescriber_number[k])
+
+presnum_time = time.time()
+print ('Prescriber number took {0} seconds to run!'.format(round((presnum_time-data_time),4)))
+
+
+'''
+Calculate total drug cost (total_cost):
+
+Create a dictionary contains total drug cost for each drug.
+Print warning message if the record doesn't have a valid drug cost, and continue.
+(ValueError: could not convert string to float:)
+
+Return {drug_cost}: 
+key: 'drug_name'
+value: float(total_cost)
+'''
+# create a dictionary defaults to float
+drug_cost = defaultdict(float)
+
+# add all drug cost entries for every drug
+for k, v1, v2 in input_records:
+    try:
+        drug_cost[k] += float(v2)
+    except ValueError:
+        print('Cost for drug {0} prescribed by {1} is missing, thus treated as 0 dollar.'.format(k, v1))
+        continue
+
+drugcost_time = time.time()
+print ('Drug cost took {0} seconds to run!'.format(round((drugcost_time-presnum_time),4)))
+
+
+'''
+Create the final sorted drug table list:
+
+a. merge 2 dictionaries {drug_cost}+{prescriber_number} by the same key 'drug_name' to {drug_table}
+b. sort {drug_table} by 'drug_cost' (descending-), then by 'drug_name' (ascending+)
+
+Return: [drug_table_sorted]
+columns: [drug_name, [prescriber_number, drug_cost]]
+rows: sorted by drug_cost(-drug_table.values[1]) then by drug_name(+drug_table.keys)
+'''
+from itertools import chain
+
+# create a dictionary drug_table default to list
+drug_table = defaultdict(list)
+
+# step a.
+for a, b in chain(prescriber_number.items(), drug_cost.items()):
+    drug_table[a].append(b)
+# step b.
+drug_table_sorted = sorted(drug_table.items(), key=lambda kv: (-kv[1][1], kv[0]))
+
+table_time = time.time()
+print ('Final sorted table took {0} seconds to run!'.format(round((table_time - drugcost_time),4)))
+
+
+#  define header and write the final sorted drug table to the output dictory as top_cost_drug.txt
+output_header = ['drug_name', 'num_prescriber', 'total_cost']
+
+try:
+    with open('./output/top_cost_drug.txt', 'w', newline='') as f:
+        output_file = csv.writer(f)
+        output_file.writerow(output_header)
+        for i in range(len(drug_table_sorted)):
+            output_file.writerow([drug_table_sorted[i][0],(drug_table_sorted[i][1][0]),round(drug_table_sorted[i][1][1])])
+except IOError as e:
+    print('No permission to write file.')
+    exit()
+
+print ('In total pharmacy_counting.py took {0} seconds to run!'.format(round((time.time() - start_time),4)))
